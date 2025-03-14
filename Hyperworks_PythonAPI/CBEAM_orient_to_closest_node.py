a5c55583bae0f0f99288834234efe06c65ec9e37
Andre Silva <andre.filipe.a.silva@gmail.com>
2025-03-14 09:26:48 +0100

    First version

---
+++ Hyperworks_PythonAPI/CBEAM_orient_to_closest_node.py
@@ -0,0 +1,76 @@
+##################################################
+#              Author: André Silva               #
+##################################################
+#
+################ Version history #################
+# v1.0 - First version (Current)
+#_________________________________________________
+
+# ----IMPORTS----
+import hm
+import hm.entities as ent
+import math
+import sys
+
+# Verifies if a Hypermesh model is detected
+def detectmodel():
+    global model    
+    model = hm.Session.get_current_model()    
+    if hm.Session.model_exists(model) is True:
+        print("Valid model detected.")        
+    else:
+        print("Model not found!")
+        exit
+
+# Function to calculate angle_between_vectors(u, v):
+def angle_between_vectors(u, v):
+    dot_product = sum(i*j for i, j in zip(u, v))
+    norm_u = math.sqrt(sum(i**2 for i in u))
+    norm_v = math.sqrt(sum(i**2 for i in v))
+    cos_theta = dot_product / (norm_u * norm_v)
+    angle_rad = math.acos(cos_theta)
+    angle_deg = math.degrees(angle_rad)
+    return angle_rad, angle_deg
+
+
+# Runs model detection and requests user to select elements and nodes
+detectmodel()
+elems = hm.CollectionByInteractiveSelection(hm.Model(model),ent.Element,"Select BEAM elements to orient.")
+nodes = hm.CollectionByInteractiveSelection(hm.Model(model),ent.Node,"Select nodes for orientation reference.")
+print(f'Number of selected elems: {len(elems)}' + f' || Number os selected nodes:{len(nodes)}')
+print("-------------------------")
+# Checks if all selected elements are CBEAM, otherwise stops the code
+for element in elems:
+    if element.typename != "CBEAM":
+        sys.exit('Selected elements contain non CBEAM elements. Aborted...')
+        exit
+
+#Initialization of variables    
+stored1 = 1000.0
+stored2 = stored1
+
+# Find the distance between both nodes of CBEAM and finding the closest node from the reference list
+for element in elems:
+    for node in nodes:
+        distance1 = hm.Model(model).hm_getdistance(ent.Node,element.node1.id,node.id,element.node1.outputsystemid.id)
+        distance2 = hm.Model(model).hm_getdistance(ent.Node,element.node2.id,node.id,element.node2.outputsystemid.id)
+        if stored1 > distance1[1].distanceTotal:
+            stored1 = distance1[1].distanceTotal
+            nodeid1 = node
+            temp1 = stored1
+        if stored2 > distance2[1].distanceTotal:
+            stored2 = distance2[1].distanceTotal
+            nodeid2 = node
+            temp2 = stored2
+    stored1 = 1000.0
+    stored2 = stored1
+
+# Checks which reference distance is smallest and then uses that as the reference for the alignment node
+    if temp1 < temp2:
+        hm.Model(model).bardirectionupdate(hm.Collection(hm.Model(model),ent.Element,[element]),nodeid1,1)
+        print("Successfully oriented element " + str(element.id) + " with respect to node " + str(nodeid1.id))
+    else:
+        hm.Model(model).bardirectionupdate(hm.Collection(hm.Model(model),ent.Element,[element]),nodeid2,1)
+        print("Successfully oriented element " + str(element.id) + " with respect to node " + str(nodeid2.id))
+    print("-------------------------")
+print("-------- FINISHED -------")
