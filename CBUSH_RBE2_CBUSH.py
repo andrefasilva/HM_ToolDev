266f4896e8b0c9a88ae46eb6fe9a40d60f41ee8d
Andre Silva <andre.filipe.a.silva@gmail.com>
2025-03-26 17:46:24 +0100

    Finished Sine implementation. Script is able to calculate the primary notch. Need to be done: random notch and improvement of sine notch with replacement of repetitive tasks by functions

---
+++ CBUSH_RBE2_CBUSH.py
@@ -0,0 +1,56 @@
+##################################################
+#              Author: André Silva               #
+##################################################
+#
+################ Version history #################
+# v1.0 - First version
+# v2.0 - Calculated distance from node A of CBEAM
+# to closest node (instead of node A and B)
+#_________________________________________________
+
+# ----IMPORTS----
+import hm
+import hm.entities as ent
+import sys
+
+def cbeamorientbyref():
+    # Verifies if a Hypermesh model is detected
+    def detectmodel():
+        global model    
+        model = hm.Session.get_current_model()    
+        if hm.Session.model_exists(model) is True:
+            print("Valid Hypermesh model detected.")        
+        else:
+            print("Model not found!")
+            exit
+
+    # Runs model detection and requests user to select elements and nodes
+    detectmodel()
+    elems = hm.CollectionByInteractiveSelection(hm.Model(model),ent.Element,"Select BEAM elements to orient.")
+    nodes = hm.CollectionByInteractiveSelection(hm.Model(model),ent.Node,"Select nodes for orientation reference.")
+    print(f'Number of selected elems: {len(elems)}' + f' || Number of selected nodes:{len(nodes)}')
+    print("-------------------------")
+    # Checks if all selected elements are CBEAM, otherwise stops the code
+    for element in elems:
+        if element.typename != "CBEAM":
+            print('Selected elements contain non CBEAM elements. Review selection.')
+            sys.exit('Terminated with errors.')
+
+    #Initialization of variables    
+    stored1 = 1000.0
+
+    # Find the distance between node A of CBEAM and finding the closest node from the reference list
+    for element in elems:
+        for node in nodes:
+            distance1 = hm.Model(model).hm_getdistance(ent.Node,element.node1.id,node.id,element.node1.outputsystemid.id)
+            if stored1 > distance1[1].distanceTotal:
+                stored1 = distance1[1].distanceTotal
+                nodeid1 = node
+                temp1 = stored1
+        stored1 = 1000.0
+
+        # Orients beam based on distance -> node A of CBEAM with closest node
+        hm.Model(model).bardirectionupdate(hm.Collection(hm.Model(),ent.Element,[element.id]),nodeid1,0)
+        print("Successfully oriented element " + str(element.id) + " with respect to node " + str(nodeid1.id))  
+        print("-------------------------")
+    print("-------- FINISHED -------")
--- PrimaryNotchingCalculator.py
+++ PrimaryNotchingCalculator.py
@@ -29,11 +29,20 @@ def PrimaryNotchingCalculator(filemanager):
     # Writes the loads (detailed and max limits) to 2 csv files
     csvwriteloads(outpath)
 
-    # Gets the random loads into an array
-    getrandomlevels(inputrandom)
 
-    # Notches the responses and plots each notched profile
-    h5_notchdata(inputh5,randomspec)
+    # Notches the responses and plots each notched profile - RANDOM
+    if inputrandom != "":
+        # Gets the random loads into an array
+        getrandomlevels(inputrandom)
+        h5_notchrandom(inputh5,randomspec)
+    elif inputsine !="":   
+        # Notches the responses and plots each notched profile - Sine
+        getsinelevels(inputsine)
+        h5_notchsine(inputh5, sinespec, storeTX, storeTY, storeTZ, storeRX, storeRY, storeRZ)
+    else:
+        import sys
+        sys.exit("No file specified with inputs for Random or Sine.")
+        
 
     # --- END OF SCRIPT --- #
     print("DONE")
@@ -48,7 +57,7 @@ def loadallpaths(pathcsv):
     import pandas as pd
 
     # Set variables as global
-    global inputf06, inputh5, outpath, inputlevels, inputrandom, analysis      
+    global inputf06, inputh5, outpath, inputlevels, inputrandom, inputsine, analysis      
         
     excelbook = pd.read_excel(pathcsv, usecols="B", keep_default_na= False)
     allfileslist = excelbook['File paths'].tolist()
@@ -56,16 +65,17 @@ def loadallpaths(pathcsv):
     inputf06 = allfileslist[0]
     inputh5 = allfileslist[1]
     inputlevels = allfileslist[2]
-    inputrandom = allfileslist[3]
+    inputsine = allfileslist[3]
+    inputrandom = allfileslist[4]
 
     # Get output file paths
-    outpath = allfileslist[4]
+    outpath = allfileslist[5]
 
     if inputrandom == "":
         analysis = "sine"
     else:
         analysis = "random"
-    return inputf06, inputh5, outpath, inputlevels, inputrandom, analysis 
+    return inputf06, inputh5, outpath, inputlevels, inputrandom, inputsine, analysis 
 
 # Function to read the f06 file and extract the mass and COG
 def openf06(filepath):
@@ -151,7 +161,8 @@ def loadcomb(csvinput):
 # Function to write the loads to a csv file
 def csvwriteloads(outputpath):
     import csv
-    print(outputpath)
+    global storeTX, storeTY, storeTZ, storeRX, storeRY, storeRZ
+
     with open(str(outputpath), "w", newline="") as f:
     # creating the writer
         writer = csv.writer(f)
@@ -190,16 +201,17 @@ def csvwriteloads(outputpath):
         writer2 = csv.writer(f2)
         writer2.writerow(["FX", "FY", "FZ", "MX", "MY", "MZ"])
         writer2.writerow([abs(storeTX), abs(storeTY), abs(storeTZ), abs(storeRX), abs(storeRY), abs(storeRZ)])
+    return storeTX, storeTY, storeTZ, storeRX, storeRY, storeRZ
 
 # Function to read the h5 file data and process the SPCD forces wrt. notch limits
-def h5_notchdata(h5file,randomspec):
+def h5_notchrandom(h5file,randomspec):
     import h5py
     import numpy as np
     import matplotlib.pyplot as plt    
 
     with h5py.File(h5file, "r") as f:
         # Retrieving the SPCD node
-        SPCDgroup = f["NASTRAN/INPUT/CONSTRAINT/SPCD"][()]
+        SPCDgroup = f["NASTRAN/RESULT/NODAL/SPC_FORCE_CPLX"][()]
         SPCDnode = SPCDgroup[0][1]
 
         #Get frequency per domainID
@@ -246,6 +258,122 @@ def h5_notchdata(h5file,randomspec):
         
         plt.show()
 
+# Function to read the h5 file data and process the SPCD forces wrt. notch limits
+def h5_notchsine(h5file,inputsine, storeTX, storeTY, storeTZ, storeRX, storeRY, storeRZ):
+    import h5py
+    import numpy as np
+    import matplotlib.pyplot as plt    
+
+    with h5py.File(h5file, "r") as f:
+        # Retrieving the SPCD node
+        SPCDgroup = f["NASTRAN/RESULT/NODAL/SPC_FORCE_CPLX"][()]
+        SPCDnode = SPCDgroup[0]["ID"]
+
+        #Get frequency per domainID
+        DomainList = f["INDEX/NASTRAN/RESULT/NODAL/SPC_FORCE_CPLX"]["DOMAIN_ID"]
+
+        #Get frequency per domainID
+        Eigendata = np.array((f["NASTRAN/RESULT/DOMAINS"]["ID","TIME_FREQ_EIGR"]).tolist())
+
+        #Get SPCforce per frequency
+        SPCFcplx = np.array((f["NASTRAN/RESULT/NODAL/SPC_FORCE_CPLX"]["ID","XR","YR","ZR","RXR","RYR","RZR","DOMAIN_ID"]).tolist())
+        
+        # Masking the domains which are wrt. sine and masking the SPCD accel data only
+        MaskSPCD = np.isin(SPCFcplx[:,0],SPCDnode)
+        MaskEigen = np.isin(Eigendata[:,0],DomainList)
+
+        
+        SPCDloads = np.stack((Eigendata[MaskEigen,1],SPCFcplx[MaskSPCD,0],SPCFcplx[MaskSPCD,1],SPCFcplx[MaskSPCD,2],SPCFcplx[MaskSPCD,3],
+                    SPCFcplx[MaskSPCD,4],SPCFcplx[MaskSPCD,5],SPCFcplx[MaskSPCD,6]), axis=0)
+        
+        # Sine profiles and notch limits arranged per frequency value
+        sineXcurve = np.stack((SPCDloads[0],np.interp(SPCDloads[0],inputsine[:,0],inputsine[:,1])), axis=0)
+        sineYcurve = np.stack((SPCDloads[0],np.interp(SPCDloads[0],inputsine[:,2],inputsine[:,3])), axis=0)
+        sineZcurve = np.stack((SPCDloads[0],np.interp(SPCDloads[0],inputsine[:,4],inputsine[:,5])), axis=0)
+
+        limitFX = np.stack((SPCDloads[0],np.full((len(SPCDloads[0])),abs(storeTX))), axis=0)
+        limitFY = np.stack((SPCDloads[0],np.full((len(SPCDloads[0])),abs(storeTY))), axis=0)
+        limitFZ = np.stack((SPCDloads[0],np.full((len(SPCDloads[0])),abs(storeTZ))), axis=0)
+        limitRX = np.stack((SPCDloads[0],np.full((len(SPCDloads[0])),abs(storeRX))), axis=0)
+        limitRY = np.stack((SPCDloads[0],np.full((len(SPCDloads[0])),abs(storeRY))), axis=0)
+        limitRZ = np.stack((SPCDloads[0],np.full((len(SPCDloads[0])),abs(storeRZ))), axis=0)
+
+
+        ratioFX = np.absolute(limitFX[1]/(SPCDloads[2]*sineXcurve[1]*9.81))
+        ratioFY = np.absolute(limitFY[1]/(SPCDloads[3]*sineXcurve[1]*9.81))
+        ratioFZ = np.absolute(limitFZ[1]/(SPCDloads[4]*sineXcurve[1]*9.81))
+        ratioRX = np.absolute(limitRX[1]/(SPCDloads[5]*sineXcurve[1]*9.81))
+        ratioRY = np.absolute(limitRY[1]/(SPCDloads[6]*sineXcurve[1]*9.81))
+        ratioRZ = np.absolute(limitRZ[1]/(SPCDloads[7]*sineXcurve[1]*9.81))
+        
+        
+        ratioFX[ratioFX > 1] = 1
+        ratioFY[ratioFY > 1] = 1
+        ratioFZ[ratioFZ > 1] = 1
+        ratioRX[ratioRX > 1] = 1
+        ratioRY[ratioRY > 1] = 1
+        ratioRZ[ratioRZ > 1] = 1
+        enveloperatio = (np.stack((ratioFX,ratioFY,ratioFZ,ratioRX,ratioRY,ratioRZ), axis = 0).T).min(1)
+
+        notchedX = enveloperatio*sineXcurve[1]  
+
+
+        
+        fig, axs = plt.subplots(3, 3)        
+        axs[0, 0].set_title("Sine X Notch profiles")        
+        axs[0, 0].plot(SPCDloads[0], sineXcurve[1], label = "Input Profile")
+        axs[0, 0].plot(SPCDloads[0], notchedX, label = "Notched Profile")
+        axs[0, 0].legend(fontsize="7")
+        
+        axs[0, 1].set_title("Sine X Notch ratios")        
+        axs[0, 1].plot(SPCDloads[0], enveloperatio, 'tab:orange', label = "Envelope ratios")
+        axs[0, 1].plot(SPCDloads[0], ratioFX, label = "Ratio FX")
+        axs[0, 1].plot(SPCDloads[0], ratioFY, label = "Ratio FY")
+        axs[0, 1].plot(SPCDloads[0], ratioFZ, label = "Ratio FZ")
+        axs[0, 1].plot(SPCDloads[0], ratioRX, label = "Ratio MX")
+        axs[0, 1].plot(SPCDloads[0], ratioRY, label = "Ratio MY")
+        axs[0, 1].plot(SPCDloads[0], ratioRZ, label = "Ratio MZ")
+        axs[0, 1].legend(fontsize="7")
+
+        axs[0, 2].set_title("Notched FX")        
+        axs[0, 2].plot(limitFX[0], limitFX[1], 'tab:orange', label = "Limit FX")
+        axs[0, 2].plot(SPCDloads[0], np.absolute(notchedX*SPCDloads[2]*9.81), label = "SPC - Fx")     
+        axs[0, 2].legend(fontsize="7")
+
+        axs[1, 0].set_title("Notched FY")        
+        axs[1, 0].plot(limitFY[0], limitFY[1], 'tab:orange', label = "Limit FY")
+        axs[1, 0].plot(SPCDloads[0], np.absolute(notchedX*SPCDloads[3]*9.81), label = "SPC - FY")     
+        axs[1, 0].legend(fontsize="7")
+
+        axs[1, 1].set_title("Notched FZ")        
+        axs[1, 1].plot(limitFZ[0], limitFZ[1], 'tab:orange', label = "Limit FZ")
+        axs[1, 1].plot(SPCDloads[0], np.absolute(notchedX*SPCDloads[4]*9.81), label = "SPC - FZ")     
+        axs[1, 1].legend(fontsize="7")
+
+        axs[1, 2].set_title("Notched MX")        
+        axs[1, 2].plot(limitRX[0], limitRX[1], 'tab:orange', label = "Limit MX")
+        axs[1, 2].plot(SPCDloads[0], np.absolute(notchedX*SPCDloads[5]*9.81), label = "SPC - MX")     
+        axs[1, 2].legend(fontsize="7")
+
+        axs[2, 0].set_title("Notched MY")        
+        axs[2, 0].plot(limitRY[0], limitRY[1], 'tab:orange', label = "Limit MY")
+        axs[2, 0].plot(SPCDloads[0], np.absolute(notchedX*SPCDloads[6]*9.81), label = "SPC - MY")     
+        axs[2, 0].legend(fontsize="7")
+
+        axs[2, 1].set_title("Notched MZ")        
+        axs[2, 1].plot(limitRZ[0], limitRZ[1], 'tab:orange', label = "Limit MZ")
+        axs[2, 1].plot(SPCDloads[0], np.absolute(notchedX*SPCDloads[7]*9.81), label = "SPC - MZ")     
+        axs[2, 1].legend(fontsize="7")
+
+
+
+
+
+        for ax in fig.get_axes():
+            ax.set_xscale("linear")
+            ax.set_yscale("linear")   
+        plt.show()
+
 # Read the random input levels
 def getrandomlevels(filerandom):
     import pandas as pd
@@ -257,6 +385,21 @@ def getrandomlevels(filerandom):
     listlevels = (pd.read_csv(filerandom, usecols=[1], keep_default_na= False).to_numpy().T)[0]
     randomspec = np.vstack([listfreqs,listlevels]).T
 
+# Read the random input levels
+def getsinelevels(filesine):
+    import pandas as pd
+    import numpy as np
+
+    global sinespec
+    sinespec = []        
+    FreqX = (pd.read_csv(filesine, delimiter=',', usecols=[0], keep_default_na= False).to_numpy().T)[0]
+    FreqY = (pd.read_csv(filesine, delimiter=',', usecols=[2], keep_default_na= False).to_numpy().T)[0]
+    FreqZ = (pd.read_csv(filesine, delimiter=',', usecols=[4], keep_default_na= False).to_numpy().T)[0]
+    LevelsX = (pd.read_csv(filesine, usecols=[1], keep_default_na= False).to_numpy().T)[0]
+    LevelsY = (pd.read_csv(filesine, usecols=[3], keep_default_na= False).to_numpy().T)[0]
+    LevelsZ = (pd.read_csv(filesine, usecols=[5], keep_default_na= False).to_numpy().T)[0]
+    sinespec = np.vstack([FreqX,LevelsX,FreqY,LevelsY,FreqZ,LevelsZ]).T
+
 # Temporary - Just for debug
-mainfile = r"C:\Users\a.silva\OneDrive - Swissto12\Desktop\ToolDev\HM_ToolDev\FileManager.xlsx"
+mainfile = r"N:\YODA_I8\20_ANALYSIS\10_SINE\FileManager.xlsx"
 PrimaryNotchingCalculator(mainfile)
